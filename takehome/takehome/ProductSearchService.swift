224b9091fcc260c41a5709eb27e5caf69a9a1068
Rohan Phadke <rohanphadke@gmail.com>
2024-02-01 21:07:51 -0500

    files initialized; codable data model for products endpoint identified

---
+++ takehome/takehome/ProductSearchService.swift
@@ -0,0 +1,8 @@
+//
+//  ProductSearchService.swift
+//  takehome
+//
+//  Created by Rohan Phadke on 2/1/24.
+//
+
+import Foundation
---
+++ takehome/takehome/SearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewModel.swift
+//  takehome
+//
+//  Created by Rohan Phadke on 2/1/24.
+//
+
+import Foundation
