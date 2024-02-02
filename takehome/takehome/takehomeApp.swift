9a9b6cea4c58fec19e56af87bda9620eebc96e37
Rohan Phadke <rohanphadke@gmail.com>
2024-02-01 20:57:28 -0500

    initialize

---
+++ takehome/takehome/takehomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  takehomeApp.swift
+//  takehome
+//
+//  Created by Rohan Phadke on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct takehomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
