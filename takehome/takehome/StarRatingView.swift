6952139d1ec2c61ac9d61bf835c9819a36bc3164
Rohan Phadke <rohanphadke@gmail.com>
2024-02-01 22:13:43 -0500

    2/1 view first work; displaying price, rating, description, and add to cart button

---
+++ takehome/takehome/StarRatingView.swift
@@ -0,0 +1,18 @@
+//
+//  StarRatingView.swift
+//  takehome
+//
+//  Created by Rohan Phadke on 2/1/24.
+//
+
+import SwiftUI
+
+struct StarRatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StarRatingView()
+}
